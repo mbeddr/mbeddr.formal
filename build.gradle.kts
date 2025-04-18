3725b36c5cdfe55cda60ff54b3d8f33d93c527f7
danielratiu <ratiud@googlemail.com>
2025-04-18 13:20:11 +0200

    build.gradle: bump dependency to mps 2024.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,7 +42,7 @@ logger.info("Repository username: {}", nexusUsername)
 // Project versions
 val major = "2024"
 val minor = "1"
-val bugfix = "2"
+val bugfix = "3"
 
 fun appendOpt(str:String, pre:String) = if(!str.isEmpty()) "${pre}${str}" else ""
 
